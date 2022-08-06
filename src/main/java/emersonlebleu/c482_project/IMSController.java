e40f9cde5e7fe9b860c160963334897b4924a469
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-06 14:48:17 -0600

    Add initialization.

--- src/main/java/emersonlebleu/c482_project/IMSController.java
+++ src/main/java/emersonlebleu/c482_project/IMSController.java
@@ -1,14 +1,24 @@
 package emersonlebleu.c482_project;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class IMSController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IMSController implements Initializable {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
+
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("IMS initialized");
     }
 }
