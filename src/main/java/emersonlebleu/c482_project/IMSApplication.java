0600049d846c27828eb0646acc5e3179e79de9e5
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-06 15:28:13 -0600

    Add main view.

--- src/main/java/emersonlebleu/c482_project/IMSApplication.java
+++ src/main/java/emersonlebleu/c482_project/IMSApplication.java
@@ -11,7 +11,7 @@ public class IMSApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(IMSApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 702.0 , 395.0);
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
