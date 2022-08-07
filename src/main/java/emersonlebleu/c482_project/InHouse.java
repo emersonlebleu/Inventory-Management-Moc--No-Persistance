962fe22784700ad5f67eae2bd601889cc7b90c48
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-07 15:13:06 -0600

    Add part class and InHouse classs.

---
+++ src/main/java/emersonlebleu/c482_project/InHouse.java
@@ -0,0 +1,26 @@
+package emersonlebleu.c482_project;
+
+/**
+ * InHouse class extends the Part class
+ */
+
+/**
+ *
+ * @author Emerson Lebleu
+ */
+public class InHouse extends Part{
+    private int machineid;
+
+    public InHouse(int id, String name, Double price, int stock, int min, int max, int machineid){
+        super(id, name, price, stock, min, max);
+        this.machineid = machineid;
+    }
+
+    public int getMachineid(){
+        return machineid;
+    }
+
+    public void setMachineid(int machineid) {
+        this.machineid = machineid;
+    }
+}
