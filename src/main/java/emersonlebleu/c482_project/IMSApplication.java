ae8d0eda012805479284622ec937b9511f7e31cb
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-07 09:19:58 -0600

    Fix modify product page label.

--- src/main/java/emersonlebleu/c482_project/IMSApplication.java
+++ src/main/java/emersonlebleu/c482_project/IMSApplication.java
@@ -11,19 +11,10 @@ public class IMSApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader mainLoader = new FXMLLoader(IMSApplication.class.getResource("main_view.fxml"));
-        FXMLLoader addPartLoader = new FXMLLoader(IMSApplication.class.getResource("add_part_view.fxml"));
-        FXMLLoader modifyPartLoader = new FXMLLoader(IMSApplication.class.getResource("modify_part_view.fxml"));
-        FXMLLoader addProductLoader = new FXMLLoader(IMSApplication.class.getResource("add_product_view.fxml"));
-        FXMLLoader modifyProductLoader = new FXMLLoader(IMSApplication.class.getResource("modify_product_view.fxml"));
 
         stage.setTitle("IMS: Main");
 
         Scene mainScene = new Scene(mainLoader.load(), 895.0 , 395.0);
-        Scene addPartScene = new Scene(addPartLoader.load(), 488.0 , 463.0);
-        Scene modifyPartScene = new Scene(modifyPartLoader.load(), 895.0 , 395.0);
-        Scene addProductScene = new Scene(addProductLoader.load(), 895.0 , 395.0);
-        Scene modifyProductScene = new Scene(modifyProductLoader.load(), 895.0 , 395.0);
-
         stage.setScene(mainScene);
         stage.show();
     }
