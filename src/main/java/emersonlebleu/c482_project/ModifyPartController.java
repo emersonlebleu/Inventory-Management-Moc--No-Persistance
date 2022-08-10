2384f82596eee4871887b621cfc808acc2b7f7c6
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-10 10:29:42 -0600

    Logic for modify part is complete.

--- src/main/java/emersonlebleu/c482_project/ModifyPartController.java
+++ src/main/java/emersonlebleu/c482_project/ModifyPartController.java
@@ -53,40 +53,51 @@ public class ModifyPartController implements Initializable {
 
     public void on_in_house(ActionEvent actionEvent) {
         machine_company.setText("Machine ID");
-        int newMachineId = Integer.parseInt(toggleField.getText());
-
     }
 
     public void on_outsourced(ActionEvent actionEvent) {
         machine_company.setText("Company Name");
-        String newCompany = toggleField.getText();
-
     }
-    private void set_fields(){
+    private void set_fields(Part part){
         int newId = Integer.parseInt(idField.getText());
+        part.setId(newId);
         String newName = nameField.getText();
-        selectedPart.setName(newName);
+        part.setName(newName);
         int newInv = Integer.parseInt(invField.getText());
-        selectedPart.setStock(newInv);
+        part.setStock(newInv);
         Double newPrice = Double.parseDouble(priceField.getText());
-        selectedPart.setPrice(newPrice);
+        part.setPrice(newPrice);
         int newMax = Integer.parseInt(maxField.getText());
-        selectedPart.setMax(newMax);
+        part.setMax(newMax);
         int newMin = Integer.parseInt(minField.getText());
-        selectedPart.setMin(newMin);
+        part.setMin(newMin);
     }
 
     public void on_save(ActionEvent actionEvent) throws IOException {
 
         //Checks if a part has changed from outsourced or in house and performs re-assignment
         if (selectedPart instanceof Outsourced && add_part_toggle.getSelectedToggle() == outsourced_radio) {
-            set_fields();
+            set_fields(selectedPart);
             String newCompany = toggleField.getText();
             ((Outsourced) selectedPart).setCompanyName(newCompany);
         } else if (selectedPart instanceof InHouse && add_part_toggle.getSelectedToggle() == in_house_radio) {
-            set_fields();
+            set_fields(selectedPart);
             int newMachineId = Integer.parseInt(toggleField.getText());
             ((InHouse) selectedPart).setMachineid(newMachineId);
+        } else if (selectedPart instanceof InHouse && add_part_toggle.getSelectedToggle() == outsourced_radio) {
+            Inventory.deletePart(selectedPart);
+            Outsourced changedPart = new Outsourced(0, "none", 0.00, 0, 0, 0, "none");
+            set_fields(changedPart);
+            String newCompany = toggleField.getText();
+            changedPart.setCompanyName(newCompany);
+            Inventory.addPart(changedPart);
+        } else if (selectedPart instanceof Outsourced && add_part_toggle.getSelectedToggle() == in_house_radio) {
+            Inventory.deletePart(selectedPart);
+            InHouse changedPart = new InHouse(0, "none", 0.00, 0, 0, 0, 0000);
+            set_fields(changedPart);
+            int newMachineId = Integer.parseInt(toggleField.getText());
+            changedPart.setMachineid(newMachineId);
+            Inventory.addPart(changedPart);
         }
 
         Parent root = FXMLLoader.load(getClass().getResource("main_view.fxml"));
