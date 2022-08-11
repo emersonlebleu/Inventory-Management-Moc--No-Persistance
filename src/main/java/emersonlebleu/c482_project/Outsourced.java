e72c216d1328ca074d04c9f6cbc2fd0d6ff38744
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-11 06:48:43 -0600

    Add docs for the two files.

--- src/main/java/emersonlebleu/c482_project/Outsourced.java
+++ src/main/java/emersonlebleu/c482_project/Outsourced.java
@@ -1,12 +1,8 @@
 package emersonlebleu.c482_project;
-/**
- * Outsourced class extends the Part class
- */
+/** Outsourced class extends the Part class. This class extends the part class by adding companyName
+  and a getter and setter method for this additional variable. */
 
-/**
- *
- * @author Emerson Lebleu
- */
+/** @author Emerson Lebleu */
 public class Outsourced extends Part{
     private String companyName;
 
@@ -15,10 +11,14 @@ public class Outsourced extends Part{
         this.companyName = companyName;
     }
 
+    /** This method gets the companyName and returns it.
+      @return the companyName which is a string*/
     public String getCompanyName(){
         return companyName;
     }
 
+    /** This method takes a parameter and sets that as the company name.
+      @param companyName sets the companyName */
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
--- src/main/java/emersonlebleu/c482_project/Product.java
+++ src/main/java/emersonlebleu/c482_project/Product.java
@@ -2,7 +2,9 @@ package emersonlebleu.c482_project;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+/** This class creates a product instance. */
 
+/** @author Emerson Lebleu */
 public class Product {
     private ObservableList<Part> associatedParts = FXCollections.observableArrayList();
     private int id;
@@ -20,62 +22,68 @@ public class Product {
         this.min = min;
         this.max = max;
     }
-
+    /** Gets and returns the int id.
+     @return the id */
     public int getId() { return id; }
 
+    /** Takes an int and sets the id.
+      @param id the id to set */
     public void setId(int id) { this.id = id; }
 
+    /** Returns a string name.
+      @return the name */
     public String getName() { return name; }
 
+    /** Takes a string and sets name.
+      @param name the name to set */
     public void setName(String name) { this.name = name; }
-    /**
-     * @return the price
-     */
+
+    /** Returns a double representing price.
+      @return the price */
     public double getPrice() { return price; }
 
-    /**
-     * @param price the price to set
-     */
+    /** Takes a double and sets the price property.
+      @param price the price to set */
     public void setPrice(double price) { this.price = price; }
 
-    /**
-     * @return the stock
-     */
+    /** Returns an int representing number of particular item said to be "in stock".
+      @return the stock */
     public int getStock() { return stock; }
 
-    /**
-     * @param stock the stock to set
-     */
+    /** Takes an int and sets the stock property.
+      @param stock the stock to set */
     public void setStock(int stock) { this.stock = stock; }
 
-    /**
-     * @return the min
-     */
+    /** Returns an int the minimum.
+      @return the min */
     public int getMin() { return min; }
 
-    /**
-     * @param min the min to set
-     */
+    /** Takes an int sets minimum to this.
+      @param min the min to set */
     public void setMin(int min) { this.min = min; }
 
-    /**
-     * @return the max
-     */
+    /** Returns an int the maximum.
+      @return the max */
     public int getMax() { return max; }
 
-    /**
-     * @param max the max to set
-     */
+    /** Takes an int sets maximum to this.
+      @param max the max to set */
     public void setMax(int max) { this.max = max; }
 
+    /** Takes a part and adds it to a list of associated parts.
+      @param part the part to add */
     public void addAssociatedPart(Part part) {
         associatedParts.add(part);
     }
 
+    /** Takes a part and removes that part from the list of associated parts.
+      @param selectedPart the part to delete */
     public boolean deleteAssociatedPart(Part selectedPart) {
         return associatedParts.remove(selectedPart);
     }
 
+    /** Returns the list of associated parts.
+      @return an ObservableList of parts */
     public ObservableList<Part> getAllAssociatedParts() {
         return associatedParts;
     }
