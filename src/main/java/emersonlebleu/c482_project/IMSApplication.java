a4dfc8a7ff2f72f74bda7f3300124d8b9265f0ad
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-12 17:25:57 -0600

    Add description of a future imporovement.

--- src/main/java/emersonlebleu/c482_project/IMSApplication.java
+++ src/main/java/emersonlebleu/c482_project/IMSApplication.java
@@ -22,7 +22,15 @@ public class IMSApplication extends Application {
         stage.show();
     }
 
-    /** Main method. Entry point into the application. */
+    /** Main method. Entry point into the application.
+     *
+     * FUTURE ENHANCEMENT: For parts, they can be InHouse or Outsourced and each of those has an additional field either
+     * MachineId or Company Name. Currently, for Outsourced parts, because company name is a String input it is not incorrect for it to be a
+     * string of numbers "1234" for instance. It is however very unlikely that a company might be named only numbers,
+     * and it could easily get stored as such in error when one changes the type of the part from InHouse to Outsourced.
+     * In the future I would like to validate this with some regular expression or some criteria in the logic
+     * of the AddPart and ModifyPart controllers. For instance, a company name could start with a letter or number,
+     * must contain letters, can contain spaces or numbers as well, and can end with a letter or number. */
     public static void main(String[] args) {
         launch();
     }
